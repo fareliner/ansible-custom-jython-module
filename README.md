42f1d741f577a1a847be0d9ef4f839b2ce0f1d09
Niels Bertram <niels.bertram@virginaustralia.com>
2016-05-27 11:31:52 +1000

    added simple example

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# ansible-custom-jython-module
-Example custom ansible module to manage WildFly/JBoss AS through jython cli
+# Example Custom Ansible Module executed with Jython Interpreter
+
+This is an example custom ansible module to manage WildFly/JBoss AS through jython cli.
+
+## Run It
+
+ensure requirements are met (see below) then run the example:
+
+```sh
+ansible-playbook -vvvv local.yml
+```
+
+## Requirements
+
+1. Target host will need to have jython installed (for this example jython needs to be present on local)
+
+2. jython executable needs to be linked into `/usr/bin` to ensure the interpreter is found by `#! /usr/bin/env jython`
+
+## Limitations
+
+- Tested successfully with ansible 2.0.0.2 but not working on ansible 2.1 anymore
+
+- To get logging working in a remote Linux host, one will need to also compile [syslog.java](https://gist.github.com/bertramn/3a57c6d513cd1e66f8606ceb392cba7b) and enable UDP rsyslog
---
+++ library/wildfly.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env jython
+
+DOCUMENTATION = '''
+---
+module: wildfly
+short_description: Manage wildfly container via jboss-cli
+'''
+
+import traceback
+from java.lang import IllegalStateException
+
+def _normalize_dirpath(dirpath):
+    return dirpath[:-1] if dirpath[-1] == '/' else dirpath
+
+def _prepare_cli_classpath(wildfly_home):
+    '''
+    jboss CLI does something dodgy and can't just append the cli.jar to the system path
+    sys.path.append(wildfly_home + "/bin/client/jboss-cli-client.jar")
+    instead we are going to add a URL classloader into the loader hierarchy of
+    the current thread context
+    '''
+    from java.lang import ClassLoader, Thread
+    from java.net import URL, URLClassLoader
+    from java.io import File
+    from jarray import array
+    cliJar = File(wildfly_home + "/bin/client/jboss-cli-client.jar").toURL()
+    currentThreadClassLoader = Thread.currentThread().getContextClassLoader()
+    urlClassLoader = URLClassLoader(array([cliJar], URL), currentThreadClassLoader)
+    Thread.currentThread().setContextClassLoader(urlClassLoader)
+
+def _normalize_dirpath(dirpath):
+    return dirpath[:-1] if dirpath[-1] == '/' else dirpath
+
+def main():
+    global module
+    module = AnsibleModule(
+        argument_spec = dict(
+            wildfly_home = dict(required=True, type='str')
+        )
+    )
+    wildfly_home = _normalize_dirpath(module.params['wildfly_home'])
+
+    _prepare_cli_classpath(wildfly_home)
+
+    changed = False
+
+    # create a cli session
+    from org.jboss.as.cli.scriptsupport import CLI
+    cli = CLI.newInstance()
+    try:
+        cli.connect()
+        # got a connection
+        result = {
+            "changed": changed,
+            "msg": "TODO add finished changes",
+            "has_syslog": HAS_SYSLOG
+        }
+        module.exit_json(**result)
+    except IllegalStateException as e:
+        result = {
+            "msg": "failed connecting to wildfly: %s" % (e.getMessage()),
+            "traceback": traceback.format_exc()
+        }
+        module.fail_json(**result)
+    finally:
+        try:
+            cli.disconnect()
+        except:
+            pass
+
+from ansible.module_utils.basic import *
+
+if __name__ == '__main__':
+    main()
