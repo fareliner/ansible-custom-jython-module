c11e7b63d8445bb660ad67677523ef66549e96d7
Niels Bertram <niels.bertram@virginaustralia.com>
2016-05-27 17:14:32 +1000

    added connection retry feature

--- library/wildfly.py
+++ library/wildfly.py
@@ -7,7 +7,59 @@ short_description: Manage wildfly container via jboss-cli
 '''
 
 import traceback
+import time
+from datetime import datetime
 from java.lang import IllegalStateException
+
+def retry(exceptions=None, tries=None):
+    if exceptions:
+        exceptions = tuple(exceptions)
+
+    def wrapper(fun):
+        def retry_calls(*args, **kwargs):
+            if tries:
+                for _ in xrange(tries):
+                    try:
+                        fun(*args, **kwargs)
+                    except exceptions:
+                        pass
+                    else:
+                        break
+            else:
+                while True:
+                    try:
+                        fun(*args, **kwargs)
+                    except exceptions:
+                        pass
+                    else:
+                        break
+
+        return retry_calls
+
+    return wrapper
+
+# need to deal with: Already connected to server
+
+@retry([IllegalStateException], 10)
+def connect(cli, controller_host=None, controller_port=None, admin_username=None, admin_password=None):
+    try:
+        module.log("trying to connect")
+        if controller_host is None or controller_port is None or admin_username is None or admin_password is None:
+            cli.connect()
+        else:
+            cli.connect(controller_host, controller_port, admin_username, admin_password)
+    except IllegalStateException as e:
+        if e.getMessage().startswith('Already connected to server'):
+            module.log("already connected to server")
+        else:
+            module.log("connecting to server failed, retry in 2 seconds: %s" % e)
+            # cleanup and try again
+            try:
+                cli.disconnect()
+            except:
+                pass
+            time.sleep( 2 )
+            raise e
 
 def _normalize_dirpath(dirpath):
     return dirpath[:-1] if dirpath[-1] == '/' else dirpath
@@ -50,7 +102,12 @@ def main():
     from org.jboss.as.cli.scriptsupport import CLI
     cli = CLI.newInstance()
     try:
-        cli.connect()
+        connect(cli)
+        module.log('after connect one')
+        cli.cmd('cd /')
+        connect(cli)
+        module.log('after connect two')
+
         # got a connection
         result = {
             "changed": False,
